b649e55c75aa189b3f1fb648d88be03c745d7054
Byron Griffith <byrongriffith91@gmail.com>
2017-11-14 16:51:58 -0500

    inquirer added

---
+++ bamazonCustomer.js
@@ -0,0 +1,61 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+var cli_table = require("cli-table");
+
+// create the connection information for the sql database
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "fourfour2",
+  database: "bamazon"
+});
+
+// connect to the mysql server and sql database
+connection.connect(function(err) {
+  if (err) throw err;
+  // run the start function after the connection is made to prompt the user
+  console.log("Welcome to Bamazon!");
+  buy();
+});
+
+function buy() {
+  // query the database for all items being auctioned
+  connection.query("SELECT * FROM products", function(err, results) {
+    if (err) throw err;
+    // once you have the items, prompt the user for which they'd like to bid on
+    inquirer
+      .prompt([
+        {
+          name: "choice",
+          type: "list",
+          choices: function() {
+            var choiceArray = [];
+            for (var i = 0; i < results.length; i++) {
+              choiceArray.push(results[i].product_name);
+            }
+            return choiceArray;
+          },
+          message: "What Would you like to buy?"
+        },
+        {
+          name: "amount",
+          type: "input",
+          message: "How many would you like to buy?"
+        }
+      ])
+      .then(function(answer) {
+        // get the information of the chosen item
+        var chosenItem;
+        for (var i = 0; i < results.length; i++) {
+          if (results[i].product_name === answer.choice) {
+            chosenItem = results[i];
+          		}
+        	}
+		});
+  });
+}
